c69e533318e24a21f54adc4827f32b5b8f8882aa
Fauzi <fauzi.adithya@gmail.com>
2019-07-24 21:54:17 +0700

    Updates

---
+++ precache-manifest.c6297c95f95f2be7d0d20591594f8ead.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed9c5a9d469f6496b8a7a64b8112cad",
+    "url": "/PokeReact/index.html"
+  },
+  {
+    "revision": "91b2babdcb5f102300d8",
+    "url": "/PokeReact/static/css/main.595473dd.chunk.css"
+  },
+  {
+    "revision": "2f19aea283a3cf382716",
+    "url": "/PokeReact/static/js/2.7fcc6ae2.chunk.js"
+  },
+  {
+    "revision": "91b2babdcb5f102300d8",
+    "url": "/PokeReact/static/js/main.77b5636f.chunk.js"
+  },
+  {
+    "revision": "3c8e8b1c25d276c24cad",
+    "url": "/PokeReact/static/js/runtime~main.6f69b682.js"
+  }
+]);
